e8368ec0b213cec10f788bc557b31e6af142ce46
Victor Milagres <victor.pmilagres@gmail.com>
2021-04-19 13:55:59 -0300

    App

---
+++ pokedex/src/router/Router.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import DetailsPokemon from '../pages/DetailsPokemon/DetailsPokemon'
+import Home from '../pages/Home/Home'
+import ListPokemon from '../pages/ListPokemon/ListPokemon'
+
+
+
+const Router = () => {
+    return (
+
+    )
+}
+
+export default Router
