013ace08e7647e10c476480d64d7d5306cd4ca5f
Janaylla Firmino <janayllaf@gmail.com>
2021-04-20 15:12:26 -0300

    Estilizações e use context

--- pokedex/src/components/pokecard/PokeCard.js
+++ pokedex/src/components/pokecard/PokeCard.js
@@ -3,13 +3,14 @@ import {DivContainer, Button2, DivButtons, Img, DivImage} from "./styled"
 import {goToDetails} from '../../router/coordinator'
 import {useHistory} from 'react-router-dom'
 
-export const PokeCard = () => {
+export const PokeCard = ({name, photo}) => {
     const history = useHistory();
+    console.log(name, photo)
     return <DivContainer>
         <DivImage>
-        <Img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"}/>
+        <Img src={photo}/>
         </DivImage>
-        <h3>Bulbassauro</h3>
+        <h3>{name}</h3>
         <DivButtons>   
         <Button2 color="secondary" variant="contained" size="small"  >Adicionar</Button2>   
         <Button2 color="secondary" variant="contained" size="small"  onClick={() => goToDetails(history, "as")}> Detalhes </Button2> 
--- pokedex/src/pages/Home/Home.js
+++ pokedex/src/pages/Home/Home.js
@@ -1,18 +1,31 @@
-import React from "react"
+import React, {useContext} from "react"
 import {PokeCard} from "../../components/pokecard/PokeCard"
 import {Header} from "../../components/header/Header"
 import styled from "styled-components"
+import PokemonsContext from "../../contexts/PokemonsContext";
+import { ListItemText } from "@material-ui/core";
+import ListPokemon from "../ListPokemon/ListPokemon";
 
 const DivContainer = styled.div`
     * {margin: 0;
     padding: 0;
     box-sizing: border-box;}
 `
+const ListPokemons = styled.div`
+display: flex;
+flex-wrap: wrap;
 
+`
 const Home = () => {
+    const pokemonsContext = useContext(PokemonsContext);
     return <DivContainer>
         <Header page="Home" />
-        <PokeCard> </PokeCard>
+        <ListPokemons>
+       {pokemonsContext.map((item) => {
+              return (<PokeCard photo={item['sprites'].front_default} name={item['name']}> </PokeCard>)
+       })
+       }
+       </ListPokemons>
     </DivContainer>
 }
 
