3bc1bb1b13b73384979469150bd83a52410a480a
Victor Milagres <victor.pmilagres@gmail.com>
2021-04-20 17:00:37 -0300

    Código

--- pokedex/src/components/pokecard/PokeCard.js
+++ pokedex/src/components/pokecard/PokeCard.js
@@ -1,12 +1,12 @@
 import React from "react"
-import {DivContainer, Button2, DivButtons, Img, DivImage} from "./styled"
+import {DivContainerCard, Button2, DivButtons, Img, DivImage} from "../../globalStyled"
 import {goToDetails} from '../../router/coordinator'
 import {useHistory} from 'react-router-dom'
 
 export const PokeCard = ({name, photo}) => {
     const history = useHistory();
     console.log(name, photo)
-    return <DivContainer>
+    return <DivContainerCard>
         <DivImage>
         <Img src={photo}/>
         </DivImage>
@@ -15,5 +15,5 @@ export const PokeCard = ({name, photo}) => {
         <Button2 color="secondary" variant="contained" size="small"  >Adicionar</Button2>   
         <Button2 color="secondary" variant="contained" size="small"  onClick={() => goToDetails(history, "as")}> Detalhes </Button2> 
         </DivButtons>
-</DivContainer>
+</DivContainerCard>
 }
